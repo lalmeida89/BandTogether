f523cfd497dcd0f2280093d16e3f11ee1b0de544
lalmeida89 <lalmeida89@gmail.com>
2017-12-08 02:49:08 -0500

    added genres, bio, and name to customize page

--- app/routes.js
+++ app/routes.js
@@ -100,6 +100,16 @@ module.exports = function(app, passport) {
             failureFlash : true // allow flash messages
         }));
 
+        app.get('/customize', function(req, res) {
+            res.render('customize.ejs', { message: req.flash('signupMessage') });
+        });
+
+        app.post('/customize', passport.authenticate('local-signup', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/customize', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+        }));
+
     // facebook -------------------------------
 
         // send to facebook to do the authentication
