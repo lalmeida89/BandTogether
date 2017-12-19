27afc03b16fbf8ee51bde34a2fc68b5ffe1b78fb
lalmeida89 <lalmeida89@gmail.com>
2017-12-19 14:29:04 -0500

    mlab stuff

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://bandtogether:bandtogether@ds155644.mlab.com:55644/bandtogether' //'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
