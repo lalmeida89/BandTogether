c4d00fd9e1bd10ba05882a00d92fc7dd322a20ef
lalmeida89 <lalmeida89@gmail.com>
2017-12-30 18:15:54 -0500

    final rough draft

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 // config/database.js
 module.exports = {
 
-    //'url' : 'mongodb://localhost/passport'
-    'url' : 'mongodb://bandtogether:bandtogether@ds155644.mlab.com:55644/bandtogether'
+    'url' : 'mongodb://localhost/passport'
+    //'url' : 'mongodb://bandtogether:bandtogether@ds155644.mlab.com:55644/bandtogether'
      // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
